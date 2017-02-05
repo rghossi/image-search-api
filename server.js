136355b7b03a8dc8b74a9370aabe3c301c9cf8a2
rghossi <rafael.ghossi@gmail.com>
2017-02-04 23:56:11 -0200

    initial project configuration

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+var searchController = require('./searchController');
+var mongoose = require('mongoose');
+
+var MONGODB_URI = process.env.database || 'mongodb://localhost/imagesearch';
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function(){
+  console.log("Successfully connected to mongodb!")
+})
+
+mongoose.connect(MONGODB_URI);
+
+app.get('/', function (req, res) {
+	res.send("Hello");
+});
+
+app.listen(PORT, function () {
+  console.log('App listening on port ' + PORT + '!');
+});
